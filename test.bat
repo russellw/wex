f1f1d7ee98a2ea3c8878fbcc94d53f7c974a2b5d
Russell Wallace <russell.wallace@gmail.com>
2025-07-14 21:31:36 +0100

    Modified 1 file(s): test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,5 @@
 rd /s /q \t\workspace
 md \t\workspace||exit /b
-python run_engine.py --workspace \workspace %1|tee log.txt
+python run_engine.py --workspace \t\workspace "write a file called hello.txt"|tee log.txt
+dir \t\workspace|tee -a log.txt
 
