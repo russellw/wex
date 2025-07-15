c3d9508da8f0ea06cdaa0062b59e72e64d443438
Russell Wallace <russell.wallace@gmail.com>
2025-07-15 20:02:35 +0100

    Added 1 file(s): test_prompts.txt; Modified 1 file(s): test.bat

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 rd /s /q \t\workspace
 md \t\workspace||exit /b
-python run_engine.py --workspace \t\workspace "write a desk calculator in C"|tee log.txt
+python run_engine.py --workspace \t\workspace --file test_prompts.txt|tee log.txt
 dir \t\workspace|tee -a log.txt
 
